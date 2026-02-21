3de49f62b03c177763935be41a900d93dc3cf62f
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-02-21 13:43:11 -0500

    feat: export prisma to use in backend

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+//export our prisma to be used elsewhere
+
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});

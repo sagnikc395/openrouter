dbb99e4d6bddb3bf6a86968553efcc28afee9522
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-04-11 03:49:14 -0400

    feat: create the payments endpoint

---
+++ apps/backend/src/modules/payments/index.ts
@@ -0,0 +1,50 @@
+import Elysia from "elysia";
+import { PaymentsModel } from "./models";
+import { PaymentsService } from "./service";
+import jwt from "@elysiajs/jwt";
+
+export const app = new Elysia({ prefix: "payments" })
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRET!,
+    }),
+  )
+  .resolve(async ({ cookie: { auth }, status, jwt }) => {
+    if (!auth) {
+      return status(401);
+    }
+
+    const decoded = await jwt.verify(auth.value as string);
+
+    if (!decoded || !decoded.userId) {
+      return status(401);
+    }
+
+    return {
+      userId: decoded.userId as string,
+    };
+  })
+
+  .post(
+    "/onramp",
+    async ({ userId, status }) => {
+      try {
+        const credits = await PaymentsService.onramp(Number(userId));
+        return {
+          message: "Onramp successfull" as const,
+          credits,
+        };
+      } catch (e) {
+        return status(411, {
+          message: "Onramp Failed" as const,
+        });
+      }
+    },
+    {
+      response: {
+        200: PaymentsModel.onrampSuccessResponseSchema,
+        411: PaymentsModel.onrampFailedResponseSchema,
+      },
+    },
+  );
